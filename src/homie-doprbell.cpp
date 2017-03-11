e3c6499c07a96a9b382f136bcca343a954aef09f
Ilan Koch <github@ilankoch.de>
2017-03-11 19:15:16 +0100

    changed threshold

--- src/homie-doprbell.cpp
+++ src/homie-doprbell.cpp
@@ -4,32 +4,36 @@
 unsigned long previousMillis = 0;
 unsigned long lastTime = 0;
 int delayValue = 200;
+int bit = 0;
 
 //type has to be an ESH type, like ESH:Switch or ESH:PowerOutlet
 HomieNode bellNode("doorbell", "ESH:Alarm");
-
-
-void loopHandler() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= delayValue){
-    int bellState = analogRead(A0);
-    Serial.println(bellState);
-    if (bellState > 500 && currentMillis - lastTime >= 2000){
-      lastTime = millis();
-      Serial.println("Ding Dong");
-      bellNode.setProperty("gong").send("TRUE");
-      delay(10);
-      bellNode.setProperty("gong").send("FALSE");
-    }
-    previousMillis = currentMillis;
-  }
-
-}
 
 void setupPublish(){
   bellNode.setProperty("").send("gong,itemtype");
   bellNode.setProperty("gong").send("FALSE");
   bellNode.setProperty("itemtype").send("Switch");
+}
+
+void loopHandler() {
+  if (bit == 0){
+    setupPublish();
+    bit = 1;
+  }
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= delayValue){
+    int bellState = analogRead(A0);
+    Serial.println(bellState);
+    if (bellState > 80 && currentMillis - lastTime >= 2000){
+      lastTime = millis();
+      Serial.println("Ding Dong");
+      bellNode.setProperty("gong").send("ON");
+      delay(10);
+      bellNode.setProperty("gong").send("OFF");
+    }
+    previousMillis = currentMillis;
+  }
+
 }
 
 void setup() {
@@ -38,10 +42,9 @@ void setup() {
   Serial.println();
   pinMode(A0, INPUT);
 
-  Homie_setFirmware("Magic Doorbell", "0.0.1");
+  Homie_setFirmware("Magic Doorbell", "0.0.2");
   Homie.setLoopFunction(loopHandler);
   Homie.setup();
-  setupPublish();
 }
 
 void loop() {
